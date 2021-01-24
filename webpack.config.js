291c0849a1539ce39e42f5b7bcca1aac936f5449
yangshangzhi <yangshangzhi@yy.com>
2021-01-24 22:55:17 +0800

    feat: modify config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: argv.mode === 'production' ? 'https://wp5-nextjs-consumer.vercel.app/' : "http://localhost:8080/",
   },
 
   resolve: {
@@ -62,4 +62,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
